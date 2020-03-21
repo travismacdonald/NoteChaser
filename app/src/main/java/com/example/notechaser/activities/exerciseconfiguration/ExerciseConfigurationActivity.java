666995a8947ae1cd0075c7b6c62e4708c0be0444
travismacdonald <x2015khn@stfx.ca>
2020-03-21 10:50:34 -0300

    Migrated to AndroidX

--- app/src/main/java/com/example/notechaser/activities/exerciseconfiguration/ExerciseConfigurationActivity.java
+++ app/src/main/java/com/example/notechaser/activities/exerciseconfiguration/ExerciseConfigurationActivity.java
@@ -1,8 +1,8 @@
 package com.example.notechaser.activities.exerciseconfiguration;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.notechaser.Constants;
 import com.example.notechaser.R;
--- app/src/main/java/com/example/notechaser/activities/exerciseselect/ExerciseSelectFragment.java
+++ app/src/main/java/com/example/notechaser/activities/exerciseselect/ExerciseSelectFragment.java
@@ -2,8 +2,8 @@ package com.example.notechaser.activities.exerciseselect;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
