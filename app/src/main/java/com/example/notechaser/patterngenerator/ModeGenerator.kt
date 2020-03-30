2f5be8b86c55ba901b6f3e8591bfc20f570daa96
travismacdonald <x2015khn@stfx.ca>
2020-03-29 22:27:06 -0300

    Init scale classes

---
+++ app/src/main/java/com/example/notechaser/patterngenerator/ModeGenerator.kt
@@ -0,0 +1,17 @@
+package com.example.notechaser.patterngenerator
+
+class ModeGenerator {
+
+    public fun getMajorMode(ix: Int) {
+        // Todo
+    }
+
+    public fun getMelodicMinorMode(ix: Int) {
+        // Todo
+    }
+
+    public fun getHarmonicMinorMode(ix: Int) {
+        // Todo
+    }
+
+}
---
+++ app/src/main/java/com/example/notechaser/patterngenerator/Scale.kt
@@ -0,0 +1,3 @@
+package com.example.notechaser.patterngenerator
+
+internal data class Scale(val ixs: Int, val name: String)
