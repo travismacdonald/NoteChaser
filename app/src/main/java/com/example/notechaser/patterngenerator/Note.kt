203c5d70b1bf219f368a78c699106b596be8acca
travismacdonald <x2015khn@stfx.ca>
2020-03-30 12:24:05 -0300

    Added note name data

--- app/src/main/java/com/example/notechaser/patterngenerator/Note.kt
+++ app/src/main/java/com/example/notechaser/patterngenerator/Note.kt
@@ -1,5 +1,6 @@
 package com.example.notechaser.patterngenerator
 
 data class Note(val ix: Int) {
-    // Todo: note name
+    val nameSharp = MusicTheory.CHROMATIC_SCALE_SHARP[ix]
+    val nameFlat = MusicTheory.CHROMATIC_SCALE_FLAT[ix]
 }
