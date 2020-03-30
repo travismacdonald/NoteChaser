c2a07cf1f48092cbf8a661c4dbe53a2b0c4555fe
travismacdonald <x2015khn@stfx.ca>
2020-03-29 22:19:55 -0300

    Moved pattern construction logic out of pattern class

--- app/src/main/java/com/example/notechaser/patterngenerator/Pattern.kt
+++ app/src/main/java/com/example/notechaser/patterngenerator/Pattern.kt
@@ -1,51 +1,31 @@
 package com.example.notechaser.patterngenerator
 
-import java.util.*
-
-
-class Pattern {
-    var notes: MutableList<Note>
+internal data class Pattern(private val notes: MutableList<Note> = mutableListOf()) {
+    var size = notes.size
+        get() = notes.size
         private set
-    private var offset = 0
-    private var canInterrupt = false
+
+    // TODO: Add functionality to add and remove notes from pattern
 
     // IntervalTemplate, Key
-    constructor(template: PatternTemplate, key: Int) {
-        // This one is pretty simple
-        notes = ArrayList()
-        for (ix in template.indices) {
-            notes.add(Note(ix + key))
-        }
-    }
-
-    fun size(): Int {
-        return notes.size
-    }
-
-    fun setCanInterrupt(canInterrupt: Boolean) {
-        this.canInterrupt = true
-    }
-
-    fun canInterrupt(): Boolean {
-        return canInterrupt
-    }
-
-//    override fun equals(o: Any?): Boolean {
-//        if (this === o) return true
-//        if (o == null || javaClass != o.javaClass) return false
-//        val pattern = o as Pattern
-//        return notes == pattern.notes
+//    constructor(template: PatternTemplate, key: Int) {
+//        // This one is pretty simple
+//        notes = ArrayList()
+//        for (ix in template.indices) {
+//            notes.add(Note(ix + key))
+//        }
 //    }
 
+//    fun size(): Int {
+//        return notes.size
+//    }
 
-//    override fun toString(): String {
-//        val string = StringBuilder()
-//        for (note in notes) {
-//            string.append(note.getName())
-//            string.append(note.ix)
-//            string.append(' ')
-//        }
-//        return string.toString()
+//    fun setCanInterrupt(canInterrupt: Boolean) {
+//        this.canInterrupt = true
+//    }
+
+//    fun canInterrupt(): Boolean {
+//        return canInterrupt
 //    }
 
 }
