b0a8176db4ad0979dfb8ee4464e4bff384a1e25f
travismacdonald <x2015khn@stfx.ca>
2020-04-02 23:34:37 -0300

    class now transposes intervals to have a minimum of zero at initialization

--- app/src/main/java/com/example/notechaser/patterngenerator/PatternTemplate.kt
+++ app/src/main/java/com/example/notechaser/patterngenerator/PatternTemplate.kt
@@ -4,9 +4,19 @@ import java.io.Serializable
 
 /**
  * Class that holds untransposed patterns.
+ * List given will be transformed so that the lowest int is zero.
  */
 // todo: change serializable to parcelable
 class PatternTemplate(private val _intervals: MutableList<Int> = arrayListOf()) : Serializable {
+
+    init {
+        if (_intervals.isNotEmpty() && _intervals.min() != 0) {
+            val min = _intervals.min()!!
+            for (i in 0 until _intervals.size) {
+                _intervals[i] = _intervals[i] - min
+            }
+        }
+    }
 
     var intervals: List<Int> = _intervals
         get() = _intervals
@@ -14,8 +24,8 @@ class PatternTemplate(private val _intervals: MutableList<Int> = arrayListOf()) 
 
     var range = -1
         get() {
-            if (_intervals.isEmpty()) -1
-            return (_intervals.minBy { it }!!) - (_intervals.maxBy { it }!!)
+            if (_intervals.isEmpty()) return -1
+            else return _intervals.max()!!
         }
         private set
 
@@ -27,7 +37,7 @@ class PatternTemplate(private val _intervals: MutableList<Int> = arrayListOf()) 
         _intervals.add(interval)
     }
 
-    fun removeInterval(ix: Int) {
+    fun removeIntervalAt(ix: Int) {
         _intervals.removeAt(ix)
     }
 
