b7a593705cc93aa4179923ad65a17e32fa4157a2
travismacdonald <x2015khn@stfx.ca>
2020-04-03 22:07:18 -0300

    Changed PatternTemplate to account for untransformed and transformed intervals

--- app/src/main/java/com/example/notechaser/patterngenerator/PatternTemplate.kt
+++ app/src/main/java/com/example/notechaser/patterngenerator/PatternTemplate.kt
@@ -3,29 +3,32 @@ package com.example.notechaser.patterngenerator
 import java.io.Serializable
 
 /**
- * Class that holds untransposed patterns.
- * List given will be transformed so that the lowest int is zero.
+ * Class that holds intervals for patterns templates.
  */
 // todo: change serializable to parcelable
 class PatternTemplate(private val _intervals: MutableList<Int> = arrayListOf()) : Serializable {
 
-    init {
-        if (_intervals.isNotEmpty() && _intervals.min() != 0) {
-            val min = _intervals.min()!!
-            for (i in 0 until _intervals.size) {
-                _intervals[i] = _intervals[i] - min
-            }
-        }
-    }
-
-    var intervals: List<Int> = _intervals
+    /**
+     * Intervals are unmodified.
+     */
+    var intervalsUntransposed: List<Int> = _intervals
         get() = _intervals
+        private set
+
+    /**
+     * Intervals that are transposed so that the minimum interval is zero.
+     */
+    var intervalsTransposed: List<Int> = _intervals
+        get() {
+            if (_intervals.min() == 0) return _intervals
+            else return _intervals.map { it - _intervals.min()!! }
+        }
         private set
 
     var range = -1
         get() {
             if (_intervals.isEmpty()) return -1
-            else return _intervals.max()!!
+            else return _intervals.max()!! - _intervals.min()!!
         }
         private set
 
