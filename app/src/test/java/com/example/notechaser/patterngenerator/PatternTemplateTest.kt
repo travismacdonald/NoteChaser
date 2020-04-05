2b1cac86e57e44c2df2a7015bd36580bd948dbe2
travismacdonald <x2015khn@stfx.ca>
2020-04-04 22:53:08 -0300

    Tests passing for new PatternTemplate update

--- app/src/test/java/com/example/notechaser/patterngenerator/PatternTemplateTest.kt
+++ app/src/test/java/com/example/notechaser/patterngenerator/PatternTemplateTest.kt
@@ -5,6 +5,8 @@ import org.junit.Test
 
 class PatternTemplateTest {
 
+    /* intervalsTransposed Tests */
+
     @Test
     fun testIntervalsTransposedNone() {
         val template = PatternTemplate(arrayListOf(0, 2, 4))
@@ -21,24 +23,6 @@ class PatternTemplateTest {
     fun testIntervalsTransposedUp() {
         val template = PatternTemplate(arrayListOf(-1, 1, 3))
         assertEquals(arrayListOf(0, 2, 4), template.intervalsTransposed)
-    }
-
-    @Test
-    fun testIntervalsTransposedNoneRange() {
-        val template = PatternTemplate(arrayListOf(0, 2, 4))
-        assertEquals(4, template.range)
-    }
-
-    @Test
-    fun testIntervalsTransposedDownRange() {
-        val template = PatternTemplate(arrayListOf(1, 3, 5))
-        assertEquals(4, template.range)
-    }
-
-    @Test
-    fun testIntervalsTransposedUpRange() {
-        val template = PatternTemplate(arrayListOf(-1, 1, 3))
-        assertEquals(4, template.range)
     }
 
     @Test
@@ -59,6 +43,10 @@ class PatternTemplateTest {
         assertEquals(arrayListOf(2, 0, 4), template.intervalsTransposed)
     }
 
+
+
+    /* intervalsUntransposed Tests */
+
     @Test
     fun testIntervalsUntransposed() {
         val template = PatternTemplate(arrayListOf(2, 4, 6))
@@ -73,12 +61,80 @@ class PatternTemplateTest {
         assertEquals(arrayListOf(2, 4, 6, 0), template.intervalsTransposed)
     }
 
+
+
+    /* range Tests */
+
+    @Test
+    fun testIntervalsTransposedNoneRange() {
+        val template = PatternTemplate(arrayListOf(0, 2, 4))
+        assertEquals(4, template.range)
+    }
+
+    @Test
+    fun testIntervalsTransposedDownRange() {
+        val template = PatternTemplate(arrayListOf(1, 3, 5))
+        assertEquals(4, template.range)
+    }
+
+    @Test
+    fun testIntervalsTransposedUpRange() {
+        val template = PatternTemplate(arrayListOf(-1, 1, 3))
+        assertEquals(4, template.range)
+    }
+
+    @Test
+    fun testRangeEmptyTemplate() {
+        val template = PatternTemplate()
+        assertEquals(-1, template.range)
+    }
+
+    @Test
+    fun testRangeSingleInterval() {
+        val template = PatternTemplate(arrayListOf(1))
+        assertEquals(0, template.range)
+    }
+
+    @Test
+    fun testRangeCorrectAfterAddInterval() {
+        val template = PatternTemplate()
+        template.addInterval(4)
+        template.addInterval(10)
+        assertEquals(6, template.range)
+    }
+
+    @Test
+    fun testRangeCorrectAfterRemoveInterval() {
+        val template = PatternTemplate(arrayListOf(4))
+        template.removeIntervalAt(0)
+        assertEquals(-1, template.range)
+    }
+
+
+
+    /* addInterval tests */
+
     @Test
     fun testAddIntervalUntransposedDown() {
         val template = PatternTemplate(arrayListOf(3, 5, 7))
         template.addInterval(1)
         assertEquals(arrayListOf(3, 5, 7, 1), template.intervalsUntransposed)
     }
+
+
+
+    /* addAllInterals Tests */
+
+    @Test
+    fun testAddAllIntervals() {
+        val template = PatternTemplate()
+        template.addAllIntervals(0, 4, 7)
+        assertEquals(arrayListOf(0, 4, 7), template.intervalsTransposed)
+    }
+
+
+
+    /* removeIntervalAt tests */
 
     @Test
     fun testRemoveIntervalTransposeDown() {
