0a59e2e090b37064a0bb7630d431d9aeef8b6385
travismacdonald <x2015khn@stfx.ca>
2020-04-05 10:05:25 -0300

    Removed pattern generation files that were no longer needed

--- app/src/main/java/com/example/notechaser/patterngenerator/PatternGenerator.java
+++ app/src/main/java/com/example/notechaser/patterngenerator/PatternGenerator.java
@@ -1,13 +1,94 @@
 package com.example.notechaser.patterngenerator;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-// Todo: This will be a class that encapsulates both types of pattern generators.
-//       This is so the app only has to communicate with one class, rather than both...
-/**
- *
- */
-public interface PatternGenerator {
 
-    // Todo: addComponent() ?
+// Todo: change serializable to parcelable
+public class IntervalPatternGenerator implements Serializable {
 
+    private String mName;
+
+    private Random mRandom;
+
+    // Todo: change IntervalTemplate ta PatternTemplate
+    private List<PatternTemplate> mIntervalTemplates;
+
+    private int mLowerBound;
+
+    private int mUpperBound;
+
+    private int mMinSpaceRequired;
+
+    public IntervalPatternGenerator() {
+        this(-1, -1);
+    }
+
+    public IntervalPatternGenerator(int lowerBound, int upperBound) {
+        mIntervalTemplates = new ArrayList<>();
+        mLowerBound = lowerBound;
+        mUpperBound = upperBound;
+        mRandom = new Random();
+    }
+
+    public Pattern generatePattern() {
+        if (hasSufficientSpace()) {
+            // Pick random template
+            PatternTemplate template = mIntervalTemplates.get(mRandom.nextInt(mIntervalTemplates.size()));
+            // Pick random key
+//            int keyIx = mRandom.nextInt(mUpperBound - mLowerBound - template.getSpaceRequired() + 1) + mLowerBound;
+            // return that shit
+//            return template.generatePattern(keyIx);
+//            return new Pattern(template, keyIx);
+            return null;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public void addIntervalTemplate(PatternTemplate toAdd) {
+        mIntervalTemplates.add(toAdd);
+        mMinSpaceRequired = calcMinSpaceRequired();
+    }
+
+    public void removeIntervalTemplate(PatternTemplate toRemove) {
+        mIntervalTemplates.remove(toRemove);
+        mMinSpaceRequired = calcMinSpaceRequired();
+    }
+
+    public boolean hasSufficientSpace() {
+        return (mUpperBound - mLowerBound) >= mMinSpaceRequired;
+    }
+
+    public int getLowerBound() {
+        return mLowerBound;
+    }
+
+    public void setLowerBound(int lowerBound) {
+        mLowerBound = lowerBound;
+    }
+
+    public int getUpperBound() {
+        return mUpperBound;
+    }
+
+    public void setUpperBound(int upperBound) {
+        mUpperBound = upperBound;
+    }
+
+    private int calcMinSpaceRequired() {
+        int maxSpace = -1;
+        // Todo: better solution than linear search
+        for (PatternTemplate template : mIntervalTemplates) {
+//            final int space = template.getSpaceRequired();
+            int space = -69;
+            if (space > maxSpace) {
+                maxSpace = space;
+            }
+        }
+        return maxSpace;
+    }
 }
--- app/src/main/java/com/example/notechaser/patterngenerator/Scale.kt
+++
@@ -1,4 +0,0 @@
-package com.example.notechaser.patterngenerator
-
-// Todo: this class may not need to be used: can use a PatternTemplate instead
-internal data class Scale(val ixs: Int, val name: String)
