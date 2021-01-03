2163137d872834db7e7b2ec23dff7e69d755fe73
travismacdonald <x2015khn@stfx.ca>
2021-01-03 09:47:44 -0400

    separated some methodss into NoteFactory'

---
+++ app/src/test/java/com/cannonballapps/notechaser/models/playablegenerator/NotePlayableGeneratorTest.kt
@@ -0,0 +1,13 @@
+package com.cannonballapps.notechaser.models.playablegenerator
+
+import junit.framework.TestCase
+import org.junit.Test
+
+class SingleNotePlayableGeneratorTest : TestCase() {
+
+    @Test
+    fun `test makeSingleNotePlayableGeneratorFromDiatonicDegrees should pass`() {
+        assertTrue(true)
+    }
+
+}
