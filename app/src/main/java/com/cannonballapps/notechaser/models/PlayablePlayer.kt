33f43d725479309a30dc54f9609c7622eb632a3c
travismacdonald <x2015khn@stfx.ca>
2021-01-05 00:17:32 -0400

    refactored PlayablePlayer

--- app/src/main/java/com/cannonballapps/notechaser/models/PlayablePlayer.kt
+++ app/src/main/java/com/cannonballapps/notechaser/models/PlayablePlayer.kt
@@ -5,35 +5,24 @@ import com.cannonballapps.notechaser.playablegenerator.Playable
 import kotlinx.coroutines.*
 
 const val NUM_MILLIS_IN_MINUTE = 60000L
-const val REVERB_LEN_MILLIS = 600L
+const val REVERB_LEN_MILLIS = 1000L
 
 class PlayablePlayer(val midiPlayer: MidiPlayer2) {
 
-    var quarterNoteBpm = 30
+    var quarterNoteBpm = 120
     private val noteLenInMillis: Long
         get() = NUM_MILLIS_IN_MINUTE / quarterNoteBpm
 
-    fun playPlayable(playable: Playable): Job {
+    suspend fun playPlayable(playable: Playable) {
         // TODO: fine for now, but have to refactor later when introducing more complex playable
         val midiNumbers = playable.notes.map { it.midiNumber }
-        return GlobalScope.launch {
-            midiPlayer.playNoteSequence(midiNumbers, noteLenInMillis)
-            // Account for reverb that persists after final note has finished playing
-            delay(REVERB_LEN_MILLIS)
-        }
-
+        midiPlayer.playNoteSequence(midiNumbers, noteLenInMillis)
+        delay(REVERB_LEN_MILLIS)
     }
 
-    fun stopCurPlayable(): Job {
-        return GlobalScope.launch {
-            midiPlayer.stop()
-            // Account for reverb that persists after final note has finished playing
-            delay(REVERB_LEN_MILLIS)
-        }
-    }
-
-    fun isPlaying(): Boolean {
-        return false
+    suspend fun stopCurPlayable() {
+        midiPlayer.stop()
+        delay(REVERB_LEN_MILLIS)
     }
 
 }
