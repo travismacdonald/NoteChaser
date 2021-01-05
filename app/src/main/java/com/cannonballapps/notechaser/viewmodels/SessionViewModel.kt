402fe898327293973e3e576cc79f0c603fb260d6
travismacdonald <x2015khn@stfx.ca>
2021-01-05 00:17:49 -0400

    refactored SessionViewModel

--- app/src/main/java/com/cannonballapps/notechaser/viewmodels/SessionViewModel.kt
+++ app/src/main/java/com/cannonballapps/notechaser/viewmodels/SessionViewModel.kt
@@ -1,12 +1,9 @@
 package com.cannonballapps.notechaser.viewmodels
 
 
-import android.app.Application
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
-import cn.sherlock.com.sun.media.sound.SF2Soundbank
 import com.cannonballapps.notechaser.data.ExerciseType
-import com.cannonballapps.notechaser.models.MidiPlayer
 import com.cannonballapps.notechaser.models.PlayablePlayer
 import com.cannonballapps.notechaser.models.playablegenerator.PlayableGenerator
 import com.cannonballapps.notechaser.models.playablegenerator.PlayableGeneratorFactory
@@ -14,16 +11,18 @@ import com.cannonballapps.notechaser.musicutilities.NotePoolType
 import com.cannonballapps.notechaser.musicutilities.getModeAtIx
 import com.cannonballapps.notechaser.playablegenerator.Playable
 import com.cannonballapps.notechaser.prefsstore.PrefsStore
-import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 
 class SessionViewModel @ViewModelInject constructor(
         private val prefsStore: PrefsStore,
 ) : ViewModel() {
 
+    private val _sessionState = MutableLiveData(SessionState.INACTIVE)
+    val sessionState: LiveData<SessionState>
+        get() = _sessionState
 
     private val _curPlayable = MutableLiveData<Playable>()
     val curPlayable: LiveData<Playable>
@@ -32,27 +31,79 @@ class SessionViewModel @ViewModelInject constructor(
     private lateinit var generator: PlayableGenerator
     lateinit var playablePlayer: PlayablePlayer
 
+    private var sessionJob: Job? = null
+    private var playPlayableJob: Job? = null
+    private var cancelPlayableJob: Job? = null
+
+    fun startSession() {
+        assertSessionNotStarted()
+
+        if (this::playablePlayer.isInitialized) {
+            cancelPlayableJob?.cancel()
+            sessionJob = launchSession()
+        }
+    }
+
+    fun endSession() {
+        if (sessionJob == null) {
+            return
+        }
+        sessionJob?.cancel()
+        sessionJob = null
+
+        playPlayableJob?.cancel()
+
+        cancelPlayableJob = launchCancelPlayable()
+    }
+
     fun initGenerator(type: ExerciseType) {
         viewModelScope.launch {
             makePlayableGenerator(type)
         }
     }
 
-    fun getNextPlayable() {
-        val nextPlayable = generator.generatePlayable()
-        _curPlayable.value = nextPlayable
-        playablePlayer.playPlayable(nextPlayable)
+    override fun onCleared() {
+        super.onCleared()
+        playablePlayer.midiPlayer.stop()
+    }
+
+    private fun launchSession(): Job {
+        return viewModelScope.launch {
+
+            val nextPlayable = generator.generatePlayable()
+            _curPlayable.value = nextPlayable
+
+            playPlayableJob = launchPlayableJob(nextPlayable)
+            playPlayableJob?.join()
+
+        }
+    }
+
+    private fun launchPlayableJob(playable: Playable): Job {
+        return viewModelScope.launch {
+            _sessionState.value = SessionState.PLAYING
+            playablePlayer.playPlayable(playable)
+            _sessionState.value = SessionState.INACTIVE
+        }
+    }
+
+    private fun launchCancelPlayable(): Job {
+        return viewModelScope.launch {
+            playablePlayer.stopCurPlayable()
+            _sessionState.value = SessionState.INACTIVE
+        }
     }
 
     private fun makePlayableGenerator(type: ExerciseType) {
         when (type) {
+            // TODO: will implement other types later
             ExerciseType.SINGLE_NOTE -> makeNotePlayableGenerator()
-            else -> throw IllegalArgumentException(
-                    "Unrecognized ExerciseType given: $type"
-            )
+
+            else -> throw IllegalArgumentException("Unrecognized ExerciseType given: $type")
         }
     }
 
+    // TODO: could clean a bit
     private fun makeNotePlayableGenerator() {
         viewModelScope.launch {
             val notePoolType = prefsStore.notePoolType().first()
@@ -85,4 +136,18 @@ class SessionViewModel @ViewModelInject constructor(
         }
     }
 
+    private fun assertSessionNotStarted() {
+        if (sessionJob != null) {
+            throw IllegalArgumentException("startSession() called before endSession()")
+        }
+    }
+
+    enum class SessionState {
+        LISTENING,
+        PLAYING,
+        INACTIVE,
+    }
+
+
 }
+
