faa960c747481e842d52b7328f5f73322f0f32b3
travismacdonald <x2015khn@stfx.ca>
2021-01-05 19:26:45 -0400

    minor bug fix in playableplayer

--- app/src/main/java/com/cannonballapps/notechaser/models/PlayablePlayer.kt
+++ app/src/main/java/com/cannonballapps/notechaser/models/PlayablePlayer.kt
@@ -5,24 +5,26 @@ import com.cannonballapps.notechaser.playablegenerator.Playable
 import kotlinx.coroutines.*
 
 const val NUM_MILLIS_IN_MINUTE = 60000L
+// TODO: this is set at a pretty conservative value right now
 const val REVERB_LEN_MILLIS = 1000L
 
 class PlayablePlayer(val midiPlayer: MidiPlayer2) {
 
-    var quarterNoteBpm = 120
+    // TODO: This should be a user parameter.
+    var quarterNoteBpm = 90
     private val noteLenInMillis: Long
         get() = NUM_MILLIS_IN_MINUTE / quarterNoteBpm
 
     suspend fun playPlayable(playable: Playable) {
         // TODO: fine for now, but have to refactor later when introducing more complex playable
         val midiNumbers = playable.notes.map { it.midiNumber }
+
         midiPlayer.playNoteSequence(midiNumbers, noteLenInMillis)
         delay(REVERB_LEN_MILLIS)
     }
 
-    suspend fun stopCurPlayable() {
+    fun stopCurPlayable() {
         midiPlayer.stop()
-        delay(REVERB_LEN_MILLIS)
     }
 
 }
