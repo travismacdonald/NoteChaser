be027f89515c9d2d99924bb993e05003a7d5af7b
travismacdonald <x2015khn@stfx.ca>
2021-01-04 20:44:52 -0400

    refactored midiplayer

---
+++ app/src/main/java/com/cannonballapps/notechaser/models/MidiPlayer2.kt
@@ -0,0 +1,65 @@
+package com.cannonballapps.notechaser.models
+
+import jp.kshoji.javax.sound.midi.*
+import kotlinx.coroutines.*
+
+private const val NOTE_ON_VELOCITY = 65
+private const val NOTE_OFF_VELOCITY = 0
+
+class MidiPlayer2(private val synth: Synthesizer) {
+
+    private var curMidiJob: Job? = null
+
+    var isRunning = false
+        private set
+
+    private val receiver = synth.receiver
+
+    fun close() {
+        stop()
+        synth.close()
+    }
+
+    fun stop() {
+        curMidiJob?.cancel()
+        synth.channels[0].allNotesOff()
+    }
+
+    suspend fun playChord(chord: List<Int>, chordLenInMillis: Long) {
+        if (isRunning) {
+            stop()
+        }
+        isRunning = true
+
+        for (note in chord) {
+            noteOn(note)
+        }
+        delay(chordLenInMillis)
+        synth.channels[0].allNotesOff()
+    }
+
+    suspend fun playNoteSequence(seq: List<Int>, noteLenInMillis: Long) {
+        if (isRunning) {
+            stop()
+        }
+        isRunning = true
+
+        for (note in seq) {
+            noteOn(note)
+            delay(noteLenInMillis)
+            noteOff(note)
+        }
+
+    }
+
+    private fun noteOn(midiNumber: Int) {
+        val msg = ShortMessage(ShortMessage.NOTE_ON, midiNumber, NOTE_ON_VELOCITY)
+        receiver.send(msg, -1)
+    }
+
+    private fun noteOff(midiNumber: Int) {
+        val msg = ShortMessage(ShortMessage.NOTE_OFF, midiNumber, NOTE_OFF_VELOCITY)
+        receiver.send(msg, -1)
+    }
+
+}
