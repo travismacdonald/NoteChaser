77922929e42c33771404d635bbe061222356b8b6
travismacdonald <macdonaldtravisc@gmail.com>
2022-12-01 09:47:48 -0400

    wip: navigation

--- app/src/main/java/com/cannonballapps/notechaser/exerciselist/ExerciseListFragment.kt
+++ app/src/main/java/com/cannonballapps/notechaser/exerciselist/ExerciseListFragment.kt
@@ -1,7 +1,6 @@
 package com.cannonballapps.notechaser.exerciselist
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -16,19 +16,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavDirections
-import androidx.navigation.compose.rememberNavController
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.cannonballapps.notechaser.common.ExerciseType
-import timber.log.Timber
 
-/**
- * Separate PRs
- *     - list items component item & preview
- *     - create button item & preview
- *     - wiring view model
- */
 class ExerciseListFragment : Fragment() {
 
     override fun onCreateView(
@@ -40,7 +31,11 @@ class ExerciseListFragment : Fragment() {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 ExerciseListScreen(
-                    onNavigateToExerciseSetupFragment = { directions -> findNavController().navigate(directions) }
+                    onNavigateToExerciseSetupFragment = { exerciseType ->
+                        findNavController().navigate(
+                            ExerciseListFragmentDirections.actionExerciseListFragmentToExerciseSetupFragment(exerciseType),
+                        )
+                    },
                 )
             }
         }
@@ -49,24 +44,25 @@ class ExerciseListFragment : Fragment() {
 
 @Composable
 fun ExerciseListScreen(
-    onNavigateToExerciseSetupFragment: (NavDirections) -> Unit,
-    viewModel: ExerciseListViewModel = viewModel(),
+    onNavigateToExerciseSetupFragment: (ExerciseType) -> Unit,
 ) {
-    val navController = rememberNavController()
-
     Surface(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.wrapContentSize()) {
             FloatingActionButton(
                 modifier = Modifier
                     .width(40.dp) // TODO
                     .height(40.dp), // TODO
                 onClick = {
-                    val directions = ExerciseListFragmentDirections.actionExerciseListFragmentToExerciseSetupFragment(ExerciseType.SINGLE_NOTE)
-                    onNavigateToExerciseSetupFragment(directions)
-//                    navController.navigate(directions)
-                }
+                    /*
+                     * TODO: refactor exercise type
+                     *       should probably just be an option in the setup fragment itself
+                     */
+                    onNavigateToExerciseSetupFragment(ExerciseType.SINGLE_NOTE)
+                },
             ) {
-
+                /*
+                 * Nothing for now
+                 */
             }
         }
     }
