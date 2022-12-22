ee31b692678f9550cd91b3d1ba1d84c50923908f
travismacdonald <macdonaldtravisc@gmail.com>
2022-12-22 08:35:19 -0400

    pin ktlint version

--- .github/scripts/run-ktlint.sh
+++ .github/scripts/run-ktlint.sh
@@ -1,4 +1,5 @@
-curl -sSL https://api.github.com/repos/pinterest/ktlint/releases/latest \
+# 69975636 == v0.46.1
+curl -sSL https://api.github.com/repos/pinterest/ktlint/releases/69975636 \
     | grep "browser_download_url.*ktlint\"" \
     | cut -d : -f 2,3 \
     | tr -d \" \
