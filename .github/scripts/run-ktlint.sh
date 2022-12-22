95cef5d508e46da6809e9897737f6f9a4eef9f78
travismacdonald <macdonaldtravisc@gmail.com>
2022-12-22 08:11:40 -0400

    add lint ci check

---
+++ .github/scripts/run-ktlint.sh
@@ -0,0 +1,9 @@
+curl -sSL https://api.github.com/repos/pinterest/ktlint/releases/latest \
+    | grep "browser_download_url.*ktlint\"" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -\
+    && chmod a+x ktlint \
+    && mv ktlint /usr/local/bin/
+
+ktlint --reporter=checkstyle
