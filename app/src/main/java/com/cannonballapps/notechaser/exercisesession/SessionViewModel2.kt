b7f52fa97e9ce3c3e6c452f8e7dc2b7a6a633763
Travis MacDonald <macdonaldtravisc@gmail.com>
2023-11-19 22:44:09 -0400

    refactor: create state machine pattern for session view model

--- app/src/main/java/com/cannonballapps/notechaser/exercisesession/SessionViewModel2.kt
+++ app/src/main/java/com/cannonballapps/notechaser/exercisesession/SessionViewModel2.kt
@@ -19,14 +19,61 @@ class SessionViewModel2(
     private val prefsStore: PrefsStore,
 ) : ViewModel() {
 
-    private val _sessionState = MutableStateFlow<SessionState>(SessionState.Loading)
+    private val _sessionState = MutableStateFlow<SessionState>(SessionState.None)
     val sessionState: StateFlow<SessionState> = _sessionState.asStateFlow()
 
     private lateinit var playableGenerator: PlayableGenerator
 
+    interface SessionStateHandler<T> {
+        fun createState(): T
+        fun enterState(state: T) { /* Default no-op */ }
+    }
+
+    private val loadingStateHandler = object : SessionStateHandler<SessionState.Loading> {
+        override fun createState() = SessionState.Loading
+
+        override fun enterState(state: SessionState.Loading) {
+            viewModelScope.launch {
+                when (val generatorResult = prefsStore.playableGeneratorFlow().first()) {
+                    is ResultOf.Success -> {
+                        playableGenerator = generatorResult.value
+                        changeStateTo(preStartStateHandler.createState())
+                    }
+                    is ResultOf.Failure -> {
+                        changeStateTo(errorStateHandler.createState())
+                    }
+                }
+            }
+        }
+    }
+
+    private val errorStateHandler = object : SessionStateHandler<SessionState.Error> {
+        override fun createState() = SessionState.Error
+    }
+
+    private val preStartStateHandler = object : SessionStateHandler<SessionState.PreStart> {
+        override fun createState() = SessionState.PreStart
+
+        override fun enterState(state: SessionState.PreStart) {
+            viewModelScope.launch {
+                delay(state.millisUntilStart)
+                changeStateTo(playingQuestionHandler.createState())
+            }
+        }
+    }
+
+    private val playingQuestionHandler = object : SessionStateHandler<SessionState.PlayingQuestion> {
+        override fun createState() =
+            SessionState.PlayingQuestion(playableGenerator.generatePlayable())
+    }
+
     init {
         startStateChangeListener()
-        loadPlayableGenerator()
+        changeStateTo(loadingStateHandler.createState())
+    }
+
+    private fun changeStateTo(sessionState: SessionState) {
+        _sessionState.value = sessionState
     }
 
     private fun startStateChangeListener() {
@@ -35,54 +82,28 @@ class SessionViewModel2(
             .launchIn(viewModelScope)
     }
 
-    private fun loadPlayableGenerator() {
-        viewModelScope.launch {
-            when (val generatorResult = prefsStore.playableGeneratorFlow().first()) {
-                is ResultOf.Success -> {
-                    onPlayableGeneratorLoadSuccess(generatorResult.value)
-                }
-                is ResultOf.Failure -> {
-                    onPlayableGeneratorLoadFailure()
-                }
-            }
+    private fun onStateChanged(state: SessionState) {
+        when (state) {
+            is SessionState.Loading -> loadingStateHandler.enterState(state)
+            is SessionState.Error -> errorStateHandler.enterState(state)
+            is SessionState.PreStart -> preStartStateHandler.enterState(state)
+            is SessionState.PlayingQuestion -> playingQuestionHandler.enterState(state)
+            is SessionState.None -> { /* No-op */ }
         }
-    }
-
-    private suspend fun onStateChanged(it: SessionState) {
-        when (it) {
-            is SessionState.PreStart -> onStateChangedToPreStart(it)
-
-            is SessionState.Error,
-            is SessionState.Loading,
-            is SessionState.PlayingQuestion,
-            -> { /* No-op */ }
-        }
-    }
-
-    private suspend fun onStateChangedToPreStart(state: SessionState.PreStart) {
-        delay(state.millisUntilStart)
-        enterPlayingQuestionState()
-    }
-
-    private fun onPlayableGeneratorLoadSuccess(generator: PlayableGenerator) {
-        this.playableGenerator = generator
-        _sessionState.value = SessionState.PreStart
-    }
-
-    private fun onPlayableGeneratorLoadFailure() {
-        _sessionState.value = SessionState.Error
-    }
-
-    private fun enterPlayingQuestionState() {
-        _sessionState.value = SessionState.PlayingQuestion(playableGenerator.generatePlayable())
     }
 }
 
 sealed interface SessionState {
+
+    object None : SessionState
+
     object Loading : SessionState
+
     object Error : SessionState
+
     object PreStart : SessionState {
         const val millisUntilStart = 3_000L
     }
+
     data class PlayingQuestion(val playable: Playable) : SessionState
 }
