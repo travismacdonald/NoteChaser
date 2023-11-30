f8a849f71d0139e675ec2840bbb5cc37c01a5af0
Travis MacDonald <macdonaldtravisc@gmail.com>
2023-11-30 08:18:10 -0400

    refactor: clean up successful loading session tests

--- app/src/test/java/com/cannonballapps/notechaser/SessionViewModel2Test.kt
+++ app/src/test/java/com/cannonballapps/notechaser/SessionViewModel2Test.kt
@@ -13,9 +13,10 @@ import com.cannonballapps.notechaser.musicutilities.PitchClass
 import com.cannonballapps.notechaser.musicutilities.playablegenerator.Playable
 import com.cannonballapps.notechaser.musicutilities.playablegenerator.PlayableGenerator
 import com.cannonballapps.notechaser.musicutilities.playablegenerator.PlaybackType
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.drop
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.test.advanceTimeBy
 import org.junit.Test
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
@@ -25,7 +26,10 @@ import runStandardCoroutineTest
 import runUnconfinedCoroutineTest
 import java.lang.Exception
 import kotlin.test.assertEquals
+import kotlin.test.assertIs
+import kotlin.test.assertNotEquals
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class SessionViewModel2Test {
 
     private val playableGenerator: PlayableGenerator = mock()
@@ -67,7 +71,8 @@ class SessionViewModel2Test {
         initViewModel()
         dataLoaderFlow.send(ResultOf.Failure(Exception()))
 
-        viewModel.screenUiData.drop(1).test {
+        viewModel.screenUiData.test {
+            skipItems(1)
             assertEquals(
                 expected = SessionState.Error,
                 actual = awaitItem().state,
@@ -76,7 +81,46 @@ class SessionViewModel2Test {
     }
 
     @Test
-    fun `when playable generator loads successfully and should not play reference pitch - session state is PreStart then PlayingQuestion`() =
+    fun `when playable generator loads successfully - session state is PreStart for 3 seconds`() =
+        runStandardCoroutineTest {
+            val settings = sessionSettings(
+                sessionKey = PitchClass.C,
+                shouldPlayReferencePitch = true,
+            )
+
+            initViewModel()
+            dataLoaderFlow.send(
+                ResultOf.Success(
+                    RequiredData(
+                        playableGenerator = playableGenerator,
+                        sessionSettings = settings,
+                    )
+                )
+            )
+
+            viewModel.screenUiData.test {
+                skipItems(1)
+                assertEquals(
+                    expected = SessionState.PreStart,
+                    actual = awaitItem().state,
+                )
+            }
+
+            advanceTimeBy(3_000)
+            assertEquals(
+                expected = SessionState.PreStart,
+                actual = viewModel.screenUiData.value.state,
+            )
+
+            advanceTimeBy(1)
+            assertNotEquals(
+                actual = viewModel.screenUiData.value.state,
+                illegal = SessionState.PreStart,
+            )
+        }
+
+    @Test
+    fun `when playable generator loads successfully and should not play reference pitch - session state is PlayingQuestion after PreStart`() =
         runStandardCoroutineTest {
             val playable = playable()
             val settings = sessionSettings(
@@ -96,7 +140,9 @@ class SessionViewModel2Test {
                 )
             )
 
-            viewModel.screenUiData.drop(2).test {
+            viewModel.screenUiData.test {
+                skipItems(1)
+                assertIs<SessionState.PreStart>(awaitItem().state)
                 assertEquals(
                     expected = SessionState.PlayingQuestion,
                     actual = awaitItem().state,
@@ -107,7 +153,7 @@ class SessionViewModel2Test {
         }
 
     @Test
-    fun `when session config loads successfully and should play starting pitch - session state is PlayingReferencePitch`() =
+    fun `when session config loads successfully and should play starting pitch - session state is PlayingReferencePitch after PreStart`() =
         runStandardCoroutineTest {
             val playable = playable()
             val settings = sessionSettings(
@@ -127,11 +173,14 @@ class SessionViewModel2Test {
                 )
             )
 
-            viewModel.screenUiData.drop(2).test {
+            viewModel.screenUiData.test {
+                skipItems(1)
+                assertIs<SessionState.PreStart>(awaitItem().state)
                 assertEquals(
                     expected = SessionState.PlayingReferencePitch(PitchClass.C),
                     actual = awaitItem().state,
                 )
+                assertIs<SessionState.PlayingQuestion>(awaitItem().state)
             }
 
             verify(playablePlayer).playPlayable(PitchClass.C.toPlayable())
